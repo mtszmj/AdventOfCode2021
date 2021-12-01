47859ae60d3ed2aae7bf1f45ee9b30243b9bc39c
Mateusz <mateuszmaj@o2.pl>
2021-12-01 14:59:46 +0100

    Day 1 Task 2

---
+++ AdventOfCode2021.Tests/Day01Task2Tests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Tests
+{
+    public class Day01Task2Tests
+    {
+        
+        [Test]
+        public void returns_five_increases()
+        {
+            var input = new[]
+            {
+                199,
+                200,
+                208,
+                210,
+                200,
+                207,
+                240,
+                269,
+                260,
+                263
+            };
+
+            var result = new Day01Task2().GetDepthMeasurementIncreasesCount(input);
+
+            result.Should().Be(5);
+        }
+
+        [Test]
+        public void return_1235()
+        {
+            var input = File.ReadAllLines("Files\\Day01.txt").Select(x => int.Parse(x)).ToArray();
+
+            var result = new Day01Task2().GetDepthMeasurementIncreasesCount(input);
+
+            result.Should().Be(1235);
+        }
+    }
+}
