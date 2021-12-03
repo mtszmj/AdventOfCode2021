b8ef250218cbdd74c5cbc87e796bd173984e9e6d
Mateusz <mateuszmaj@o2.pl>
2021-12-03 21:47:32 +0100

    Day 03 Task 2

---
+++ AdventOfCode2021.Tests/Day03Task2Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2021.Tests.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Tests
+{
+    internal class Day03Task2Tests
+    {
+        [Test]
+        public void returns_230()
+        {
+            var input = @"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+            var inputLines = input.Split(Environment.NewLine);
+            var result = new Day03Task2().ReadOxygenAndCO2(inputLines);
+
+            result.Should().Be(230);
+        }
+
+        [Test]
+        public void returns_4996233()
+        {
+            var input = Helper.ReadLinesAsString(3);
+
+            var result = new Day03Task2().ReadOxygenAndCO2(input);
+
+            result.Should().Be(4996233);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day03Task2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day03Task2
+    {
+        public object ReadOxygenAndCO2(string[] inputLines)
+        {
+            var lineSize = inputLines[0].Length;
+            var mostCommon = new int[lineSize];
+            var leastCommon = new int[lineSize];
+            var oxygen = inputLines.ToImmutableArray();
+            var co2 = inputLines.ToImmutableArray();
+
+            var index = 0;
+            while(oxygen.Length > 1)
+            {
+                var result = 0;
+                foreach (var line in oxygen)
+                {
+                    _ = line[index] == '1' ? ++result : --result;
+                }
+
+                oxygen = result >= 0 
+                    ? oxygen.Where(line => line[index] == '1').ToImmutableArray() 
+                    : oxygen.Where(line => line[index] == '0').ToImmutableArray();
+
+                index = (++index) % lineSize;
+            }
+            var oxygenResult = Convert.ToInt32(oxygen[0], 2);
+
+
+            index = 0;
+            while (co2.Length > 1)
+            {
+                var result = 0;
+                foreach (var line in co2)
+                {
+                    _ = line[index] == '1' ? ++result : --result;
+                }
+
+                co2 = result >= 0
+                    ? co2.Where(line => line[index] == '0').ToImmutableArray()
+                    : co2.Where(line => line[index] == '1').ToImmutableArray();
+             
+                index = (++index) % lineSize;
+            }
+
+            var co2Result = Convert.ToInt32(co2[0], 2);
+
+            return oxygenResult * co2Result;
+        }
+    }
+}
