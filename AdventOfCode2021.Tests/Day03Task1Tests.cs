e8d6fdf2a2f76f0f2cabf8294506e81b0988c447
Mateusz <mateuszmaj@o2.pl>
2021-12-03 20:56:26 +0100

    Day 03 Task 1

---
+++ AdventOfCode2021.Tests/Day03Task1Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2021.Tests.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Tests
+{
+    internal class Day03Task1Tests
+    {
+        [Test]
+        public void returns_198()
+        {
+            var input = @"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+            var inputLines = input.Split(Environment.NewLine);
+            var result = new Day03Task1().ReadPowerConsumption(inputLines);
+
+            result.Should().Be(198);
+        }
+
+        [Test]
+        public void returns_3912944()
+        {
+            var input = Helper.ReadLinesAsString(3);
+
+            var result = new Day03Task1().ReadPowerConsumption(input);
+
+            result.Should().Be(3912944);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day03Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Day03Task1
+    {
+        public object ReadPowerConsumption(string[] inputLines)
+        {
+            var lineSize = inputLines[0].Length;
+            var mostCommonBit = new int[lineSize];
+            foreach(var line in inputLines)
+            {
+                for(var i = 0; i < lineSize; i++)
+                {
+                    _ = line[i] == '1' ? ++mostCommonBit[i] : --mostCommonBit[i];
+                }
+            }
+
+            StringBuilder gammaSb = new StringBuilder();
+            StringBuilder epsilonSb = new StringBuilder();
+            for(var i = 0; i < lineSize; i++)
+            {
+                gammaSb.Append(mostCommonBit[i] > 0 ? '1' : '0');
+                epsilonSb.Append(mostCommonBit[i] < 0 ? '1' : '0');
+            }
+
+            var gamma = Convert.ToInt32(gammaSb.ToString(), 2);
+            var epsilon = Convert.ToInt32(epsilonSb.ToString(), 2);
+
+            return gamma * epsilon;
+        }
+    }
+}
