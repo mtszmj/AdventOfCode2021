0c7f29d8ec7a9eca0b7c1ce1790edd9dee30bb51
Mateusz <mateuszmaj@o2.pl>
2021-12-06 21:28:33 +0100

    Day 6 Task 2

---
+++ AdventOfCode2021.Tests/Day06Task2Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2021.Tests;
+internal class Day06Task2Tests
+{
+    [TestCase(80, 5934)]
+    [TestCase(256, 26984457539)]
+    public void returns_fish_count(int days, long expected)
+    {
+        var input = @"3,4,3,1,2".Split(',').Select(int.Parse);
+
+        var result = new Day06Task2().HowMuchIsTheFish(input, days);
+
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void returns_361169()
+    {
+        var input = Helper.ReadFile(6).Split(',').Select(int.Parse);
+
+        var result = new Day06Task2().HowMuchIsTheFish(input, 256);
+
+        result.Should().Be(361169);
+    }
+}
---
+++ AdventOfCode2021/Day06Task2.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2021;
+public class Day06Task2
+{
+    private const int FishCycle = 6;
+    private const int NewFishCycle = 8;
+    public long HowMuchIsTheFish(IEnumerable<int> input, int days)
+    {
+        var fishCounter = InitCounter(input);
+        for(var d = 0; d< days; d++) 
+            SimulateDay(fishCounter);
+        return fishCounter.Sum();
+    }
+
+    private static void SimulateDay(long[] fishCounter)
+    {
+        var zeroValue = fishCounter[0];
+        for (var i = 0; i < NewFishCycle; i++)
+            fishCounter[i] = fishCounter[i + 1];
+
+        fishCounter[FishCycle] += zeroValue;
+        fishCounter[NewFishCycle] = zeroValue;
+    }
+
+    private static long[] InitCounter(IEnumerable<int> input)
+    {
+        var fishCounter = new long[NewFishCycle + 1];
+        foreach (var i in input)
+            fishCounter[i]++;
+        return fishCounter;
+    }
+}
