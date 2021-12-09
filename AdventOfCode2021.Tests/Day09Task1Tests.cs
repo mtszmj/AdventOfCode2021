fef2f8edb624ad8af22249ad38ea09d4cdd26259
Mateusz <mateuszmaj@o2.pl>
2021-12-09 22:20:07 +0100

    Day 9 Task 1

---
+++ AdventOfCode2021.Tests/Day09Task1Tests.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode2021.Tests;
+internal class Day09Task1Tests
+{
+    private static readonly string TestInput = @"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+    [Test]
+    public void find_low_points()
+    {
+        var solver = new Day09Task1();
+        var parsedInput = solver.ParseInput(TestInput.Split(Environment.NewLine));
+
+        parsedInput[0, 0].Row.Should().Be(0);
+        parsedInput[0, 0].Column.Should().Be(0);
+        parsedInput[0, 0].Height.Should().Be(2);
+
+        parsedInput[0, 9].Row.Should().Be(0);
+        parsedInput[0, 9].Column.Should().Be(9);
+        parsedInput[0, 9].Height.Should().Be(0);
+
+        parsedInput[4, 0].Row.Should().Be(4);
+        parsedInput[4, 0].Column.Should().Be(0);
+        parsedInput[4, 0].Height.Should().Be(9);
+
+        parsedInput[4, 9].Row.Should().Be(4);
+        parsedInput[4, 9].Column.Should().Be(9);
+        parsedInput[4, 9].Height.Should().Be(8);
+
+        parsedInput[2, 2].Row.Should().Be(2);
+        parsedInput[2, 2].Column.Should().Be(2);
+        parsedInput[2, 2].Height.Should().Be(5);
+    }
+
+    [Test]
+    public void finds_low_points()
+    {
+        var solver = new Day09Task1();
+        var lowPoints = solver.FindLowPoints(TestInput.Split(Environment.NewLine)).ToList();
+
+        lowPoints.Should().HaveCount(4);
+        lowPoints.Should().Contain(new Day09Task1.Point(0, 1, 1));
+        lowPoints.Should().Contain(new Day09Task1.Point(0, 9, 0));
+        lowPoints.Should().Contain(new Day09Task1.Point(2, 2, 5));
+        lowPoints.Should().Contain(new Day09Task1.Point(4, 6, 5));
+    }
+
+    [Test]
+    public void finds_result_15()
+    {
+        var solver = new Day09Task1();
+        var riskLevel = solver.SumRiskLevel(TestInput.Split(Environment.NewLine));
+
+        riskLevel.Should().Be(15);
+    }
+
+    [Test]
+    public void finds_result_530()
+    {
+        var solver = new Day09Task1();
+        var riskLevel = solver.SumRiskLevel(Helper.ReadLinesAsString(9));
+
+        riskLevel.Should().Be(530);
+    }
+}
---
+++ AdventOfCode2021/Day09Task1.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2021;
+public class Day09Task1
+{
+    public record Point(int Row, int Column, int Height);
+ 
+    public Point[,] ParseInput(string[] lines)
+    {
+        Point[,] points = new Point[lines.Length, lines[0].Length];
+        for (int row = 0; row < lines.Length; row++)
+            for (int col = 0; col < lines[0].Length; col++)
+                points[row, col] = new Point(row, col, lines[row][col] - '0');
+
+        return points;
+    }
+
+    public IEnumerable<Point> FindLowPoints(string[] lines)
+    {
+        var points = ParseInput(lines);
+
+        var maxRow = points.GetLength(0) - 1;
+        var maxColumn = points.GetLength(1) - 1;
+        for (var row = 0; row <= maxRow; row++)
+            for(var col = 0; col <= maxColumn; col++)
+            {
+                if(
+                    (row == 0 || (points[row, col].Height < points[row - 1, col].Height))
+                    && (col == 0 || (points[row, col].Height < points[row, col - 1].Height))
+                    && (row == maxRow || (points[row, col].Height < points[row + 1, col].Height))
+                    && (col == maxColumn || (points[row, col].Height < points[row, col + 1].Height))
+                    )
+                {
+                    yield return points[row, col];
+                }
+            }
+    }
+
+    public int SumRiskLevel(string[] lines)
+    {
+        return FindLowPoints(lines).Sum(x => x.Height + 1);
+    }
+}
