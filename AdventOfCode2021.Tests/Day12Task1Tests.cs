38257b6ad441a79b42ae5497ce291d5cd05f7ab6
Mateusz <mateuszmaj@o2.pl>
2021-12-12 11:06:21 +0100

    Day 12 Task 1

--- AdventOfCode2021.Tests/Day12Task1Tests.cs
+++ AdventOfCode2021.Tests/Day12Task1Tests.cs
@@ -1,21 +1,95 @@
 ﻿namespace AdventOfCode2021.Tests;
 internal class Day12Task1Tests
 {
-    public string Example = @"";
+    public const string Example = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+
+    public const string Example2 = @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+
+    public const string Example3 = @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+
     Day12Task1 Solver() => new();
     int Day = 12;
 
-    //[Test]
-    public void solves_example()
+    [TestCase(Example, "start,A,b,A,c,A,end")]
+    [TestCase(Example, "start,A,b,A,end")]
+    [TestCase(Example, "start,A,b,end")]
+    [TestCase(Example, "start,A,c,A,b,A,end")]
+    [TestCase(Example, "start,A,c,A,b,end")]
+    [TestCase(Example, "start,A,c,A,end")]
+    [TestCase(Example, "start,A,end")]
+    [TestCase(Example, "start,b,A,c,A,end")]
+    [TestCase(Example, "start,b,A,end")]
+    [TestCase(Example, "start,b,end")]
+    [TestCase(Example2, "start,HN,dc,HN,end")]
+    [TestCase(Example2, "start,HN,dc,HN,kj,HN,end")]
+    [TestCase(Example2, "start,HN,dc,end")]
+    [TestCase(Example2, "start,HN,dc,kj,HN,end")]
+    [TestCase(Example2, "start,HN,end")]
+    [TestCase(Example2, "start,HN,kj,HN,dc,HN,end")]
+    [TestCase(Example2, "start,HN,kj,HN,dc,end")]
+    [TestCase(Example2, "start,HN,kj,HN,end")]
+    [TestCase(Example2, "start,HN,kj,dc,HN,end")]
+    [TestCase(Example2, "start,HN,kj,dc,end")]
+    [TestCase(Example2, "start,dc,HN,end")]
+    [TestCase(Example2, "start,dc,HN,kj,HN,end")]
+    [TestCase(Example2, "start,dc,end")]
+    [TestCase(Example2, "start,dc,kj,HN,end")]
+    [TestCase(Example2, "start,kj,HN,dc,HN,end")]
+    [TestCase(Example2, "start,kj,HN,dc,end")]
+    [TestCase(Example2, "start,kj,HN,end")]
+    [TestCase(Example2, "start,kj,dc,HN,end")]
+    [TestCase(Example2, "start,kj,dc,end")]
+    public void contains_path(string input, string path)
     {
-        var result = Solver().Solve(Example);
-        result.Should().Be(0);
+        var result = Solver().Solve(input.Split(Environment.NewLine));
+        result.Any(x => x.ToString() == path).Should().BeTrue();
     }
 
-    //[Test]
+    [TestCase(Example, 10)]
+    [TestCase(Example2, 19)]
+    [TestCase(Example3, 226)]
+    public void has_paths(string input, int paths)
+    {
+        var result = Solver().Solve(input.Split(Environment.NewLine));
+        result.Count.Should().Be(paths);
+    }
+
+    [Test]
     public void solves_task()
     {
-        var result = Solver().Solve(Helper.ReadFile(Day));
-        result.Should().Be(0);
+        var result = Solver().Solve(Helper.ReadLinesAsString(Day));
+        result.Count.Should().Be(4411);
     }
 }
--- AdventOfCode2021/Day12Task1.cs
+++ AdventOfCode2021/Day12Task1.cs
@@ -1,8 +1,134 @@
 ﻿namespace AdventOfCode2021;
 public class Day12Task1
 {
-    public int Solve(string input)
+    public List<Path> Solve(string[] input)
     {
-        return -1;
+        var graph = Graph.Create(input);
+        var paths = Traverse(graph).ToList();
+        return paths;
+    }
+
+    public IEnumerable<Path> Traverse(Graph graph)
+    {
+        var path = new Path(graph.Nodes["start"]);
+        var paths = new Queue<Path>();
+        paths.Enqueue(path);
+
+        while (paths.Any())
+        {
+            var current = paths.Dequeue();
+            var lastNode = current.Nodes.Last();
+            foreach (var n in lastNode.GetConnectedNodes())
+            {
+                if(n.Name == "end")
+                    yield return new Path(current, n);
+                else if (current.CanVisit(n))
+                    paths.Enqueue(new Path(current, n));
+            }
+        }
+    }
+
+
+    public class Path
+    {
+        public Path(Node startNode)
+        {
+            Visited = new() { [startNode] = 1 };
+            Nodes = new() { startNode };
+        }
+
+        public Path(Path path, Node node)
+        {
+            Visited = new Dictionary<Node, int>(path.Visited);
+            Nodes = new List<Node>(path.Nodes);
+
+            Nodes.Add(node);
+            Visited[node] = Visited.GetValueOrDefault(node, 0) + 1;
+        }
+
+        Dictionary<Node, int> Visited { get; }
+        public List<Node> Nodes { get; }
+
+
+        public bool CanVisit(Node node)
+        {
+            return !Visited.ContainsKey(node) || Visited[node] < node.MaxVisits;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", Nodes);
+        }
+    }
+
+    public class Graph
+    {
+        public Dictionary<string,Node> Nodes { get; } = new Dictionary<string,Node>();
+
+        public static Graph Create(string[] paths)
+        {
+            var graph = new Graph();
+            foreach(var path in paths)
+            {
+                var nodes = path.Split('-', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                graph.AddConnection(nodes[0], nodes[1]);
+            }
+            return graph;
+        }
+
+        public void AddConnection(string node1, string node2)
+        {
+            Node n1 = GetOrCreateNode(node1);
+            Node n2 = GetOrCreateNode(node2);
+
+            n1.Connect(n2);
+        }
+
+        private Node GetOrCreateNode(string nodeName)
+        {
+            if (Nodes.ContainsKey(nodeName))
+                return Nodes[nodeName];
+         
+            var node = CreateNode(nodeName);
+            Nodes.Add(nodeName, node);
+            return node;
+        }
+
+        public Node CreateNode(string node)
+        {
+            if(char.IsLower(node[0]))
+                return new Node(node, 1);
+
+            return new Node(node, int.MaxValue);
+        }
+    }
+
+    public class Node
+    {
+        public Node(string name, int maxVisits)
+        {
+            Name = name;
+            MaxVisits = maxVisits;
+        }
+
+        public string Name { get; }
+        public int MaxVisits { get; }
+        protected HashSet<Node> ConnectedNodes { get; } = new HashSet<Node>();
+
+        public IEnumerable<Node> GetConnectedNodes()
+        {
+            return ConnectedNodes.ToImmutableHashSet();
+        }
+
+        public void Connect(Node node)
+        {
+            ConnectedNodes.Add(node);
+            node.ConnectedNodes.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
