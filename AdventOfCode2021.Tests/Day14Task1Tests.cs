956de16cb8989a51cab4f7bc30062d824deebec0
Mateusz <mateuszmaj@o2.pl>
2021-12-14 21:32:24 +0100

    Day 14 Task 1

--- AdventOfCode2021.Tests/Day14Task1Tests.cs
+++ AdventOfCode2021.Tests/Day14Task1Tests.cs
@@ -1,18 +1,35 @@
 ﻿namespace AdventOfCode2021.Tests;
 internal class Day14Task1Tests
 {
-    public string Example = @"";
+    public string Example = @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
     Day14Task1 Solver() => new Day14Task1();
     int Day = 14;
 
-    //[Test]
+    [Test]
     public void solves_example()
     {
         var result = Solver().Solve(Example);
-        result.Should().Be(0);
+        result.Should().Be(1588);
     }
 
-    //[Test]
+    [Test]
     public void solves_task()
     {
         var result = Solver().Solve(Helper.ReadFile(Day));
--- AdventOfCode2021/Day14Task1.cs
+++ AdventOfCode2021/Day14Task1.cs
@@ -1,8 +1,42 @@
 ﻿namespace AdventOfCode2021;
 public class Day14Task1
 {
-    public int Solve(string input)
+    public long Solve(string input)
     {
-        return -1;
+        var parts = input.Split($"{Environment.NewLine}{Environment.NewLine}");
+        var text = parts[0];
+        var rules = parts[1].Split(Environment.NewLine).Select(x => x.Split(" -> ")).ToDictionary(x => x[0], x => x[1]);
+
+        // keep track of text by storing pair of chars only - no need to know exact order of letters
+        var pairs = new Dictionary<string, long>();
+        for (var i = 0; i < text.Length - 1; i++)
+            pairs[text[i..(i + 2)]] = pairs.GetValueOrDefault(text[i..(i + 2)]) + 1;
+
+        for (var i = 0; i < 10; i++)
+        {
+            var newPairs = new Dictionary<string, long>();
+            foreach (var pair in pairs)
+            {
+                var insert = rules[pair.Key];
+                var p1 = $"{pair.Key[0]}{insert}";
+                var p2 = $"{insert}{pair.Key[1]}";
+                newPairs[p1] = newPairs.GetValueOrDefault(p1) + pair.Value;
+                newPairs[p2] = newPairs.GetValueOrDefault(p2) + pair.Value;
+            }
+            pairs = newPairs;
+        }
+
+        // count letters
+        var letterCount = new Dictionary<char, long>();
+        foreach (var pair in pairs)
+        {
+            letterCount[pair.Key[0]] = letterCount.GetValueOrDefault(pair.Key[0]) + pair.Value;
+            letterCount[pair.Key[1]] = letterCount.GetValueOrDefault(pair.Key[1]) + pair.Value;
+        }
+        // first and last letter is not counted twice (as all in the middle), therefore add 1
+        letterCount[text[0]]++;
+        letterCount[text.Last()]++;
+
+        return (letterCount.Values.Max() - letterCount.Values.Min()) / 2;
     }
 }
