4a1c568ed771517735061d3176eb4dd48a6d90b0
Mateusz <mateuszmaj@o2.pl>
2021-12-16 18:41:57 +0100

    Day 16 Task 1

--- AdventOfCode2021.Tests/Day16Task1Tests.cs
+++ AdventOfCode2021.Tests/Day16Task1Tests.cs
@@ -5,17 +5,37 @@ internal class Day16Task1Tests
     Day16Task1 Solver() => new();
     int Day = 16;
 
-    //[Test]
-    public void solves_example()
+    [TestCase("D2FE28", "110100101111111000101000")]
+    [TestCase("38006F45291200", "00111000000000000110111101000101001010010001001000000000")]
+    [TestCase("EE00D40C823060", "11101110000000001101010000001100100000100011000001100000")]
+    public void converts_to_binary(string hex, string bin)
     {
-        var result = Solver().Solve(Example);
-        result.Should().Be(0);
+        Solver().ToBinary(hex).Should().Be(bin);
     }
 
-    //[Test]
+    [TestCase("110100101111111000101000", 6)]
+    [TestCase("00111000000000000110111101000101001010010001001000000000", 9)]
+    [TestCase("11101110000000001101010000001100100000100011000001100000", 14)]
+    public void get_vesions_sum(string bin, int versions)
+    {
+        Solver().SumVersions(bin, out var end).Should().Be(versions);
+    }
+
+
+    [TestCase("8A004A801A8002F478", 16)]
+    [TestCase("620080001611562C8802118E34", 12)]
+    [TestCase("C0015000016115A2E0802F182340", 23)]
+    [TestCase("A0016C880162017C3686B18A3D4780", 31)]
+    public void solves_example(string input, int expected)
+    {
+        var result = Solver().Solve(input);
+        result.Should().Be(expected);
+    }
+
+    [Test]
     public void solves_task()
     {
         var result = Solver().Solve(Helper.ReadFile(Day));
-        result.Should().Be(0);
+        result.Should().Be(904);
     }
 }
--- AdventOfCode2021/Day16Task1.cs
+++ AdventOfCode2021/Day16Task1.cs
@@ -1,8 +1,93 @@
 ﻿namespace AdventOfCode2021;
 public class Day16Task1
 {
-    public int Solve(string input)
+    public long Solve(string input)
     {
-        return -1;
+        var bin = ToBinary(input);
+        return SumVersions(bin, out var end);
+    }
+
+    public string ToBinary(string hex)
+    {
+        var x = Convert.FromHexString(hex);
+        return string.Join("", x.Select(v => Convert.ToString(v, 2).PadLeft(8, '0')));
+    }
+
+    public long SumVersions(ReadOnlySpan<char> span, out int end)
+    {
+        end = 0;
+        var version = span[..3].ToDecimalValue();
+        var packet = span[3..6].ToDecimalValue();
+
+        if(packet == 4)
+        {
+            var index = 0;
+            var sb = new StringBuilder();
+            var plusOne = true;
+            while (plusOne)
+            {
+                sb.Append(span[(6 + index * 5 + 1)..(6 + (index + 1) * 5)]);
+                if (span[6 + index * 5] == '0')
+                    plusOne = false;
+                index++;
+            }
+            end = 6 + index * 5;
+            var value = sb.ToString().ToDecimalValue();
+
+            Console.WriteLine($"{span} [V{version}][P{packet}][Val{value}][End{end}]");
+
+            return version;
+        }
+
+        var lengthType = span[6] == '1';
+        if (!lengthType)
+        {
+            var length = span[7..(7 + 15)].ToDecimalValue();
+            var subpackets = span[(7 + 15)..(7 + 15 + (int)length)];
+
+            Console.WriteLine($"{span} [V{version}][P{packet}][Len{length}]");
+
+            var from = 0;
+            while (from < subpackets.Length)
+            {
+                Console.WriteLine($"{subpackets} [From{from}]");
+                version += SumVersions(subpackets[from..], out end);
+                from += end;
+            }
+            end = from + 22;
+            Console.WriteLine($"{subpackets} [End{end}]");
+        }
+
+        else
+        {
+            var numOfSubpackets = span[7..(7 + 11)].ToDecimalValue();
+            var subpackets = span[(7+11)..];
+            var from = 0;
+            Console.WriteLine($"{span} [V{version}][P{packet}][Num{numOfSubpackets}]");
+            for (var i = 0; i < numOfSubpackets; i++)
+            {
+                Console.WriteLine($"{subpackets} [From{from}]");
+                version += SumVersions(subpackets[from..], out end);
+                from += end;
+            }
+            end = from + 18;
+            Console.WriteLine($"{subpackets} [End{end}]");
+        }
+
+        return version;
+    }
+
+}
+
+public static class Ext
+{
+    public static long ToDecimalValue(this string bin)
+    {
+        return Convert.ToInt64(bin, 2);
+    }
+
+    public static long ToDecimalValue(this ReadOnlySpan<char> span)
+    {
+        return Convert.ToInt64(span.ToString(), 2);
     }
 }
