0b5101fd31992d8fbd561d5560fc5b1b464ca886
Mateusz <mateuszmaj@o2.pl>
2021-12-19 19:18:38 +0100

    Day 18 Task 1

--- AdventOfCode2021.Tests/Day18Task1Tests.cs
+++ AdventOfCode2021.Tests/Day18Task1Tests.cs
@@ -1,21 +1,210 @@
 ﻿namespace AdventOfCode2021.Tests;
 internal class Day18Task1Tests
 {
-    public string Example = @"";
+    public string Example = @"[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
     Day18Task1 Solver() => new Day18Task1();
     int Day = 18;
 
-    //[Test]
+    [TestCase("[1,2]", 3, 1)]
+    [TestCase("[[1,2],3]", 5, 2)]
+    [TestCase("[9,[8,7]]", 5, 2)]
+    [TestCase("[[1,9],[8,5]]", 7, 2)]
+    [TestCase("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]", 17, 4)]
+    [TestCase("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]", 21, 4)]
+    [TestCase("[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]", 31, 4)]
+    public void parses(string line, int count, int maxLevel)
+    {
+        var main = Solver().Parse(line);
+        main.Level.Should().Be(0);
+        var cnt = main.Count();
+
+        cnt.count.Should().Be(count);
+        cnt.maxLevel.Should().Be(maxLevel);
+    }
+
+    [Test]
+    public void adds_two_pairs()
+    {
+        var solver = Solver();
+        var p1 = solver.Parse("[1,2]");
+        var p2 = solver.Parse("[[3,4],5]");
+        var sum = p1.Plus(p2);
+        sum.ToString().Should().Be("[[1,2],[[3,4],5]]");
+
+        var cnt = sum.Count();
+        cnt.count.Should().Be(9);
+        cnt.maxLevel.Should().Be(3);
+    }
+
+    [TestCase("[1,2]", null)]
+    [TestCase("[[[[[9,8],1],2],3],4]", "[9,8]")]
+    [TestCase("[7,[6,[5,[4,[3,2]]]]]", "[3,2]")]
+    [TestCase("[[6,[5,[4,[3,2]]]],1]", "[3,2]")]
+    [TestCase("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[7,3]")]
+    [TestCase("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[3,2]")]
+    public void finds_pair_to_explode(string input, string expected)
+    {
+        var solver = Solver();
+        var main = solver.Parse(input);
+        var pairToExplode = main.SearchExplode();
+        if (expected is null)
+            pairToExplode.Should().BeNull();
+        else
+            pairToExplode.ToString().Should().Be(expected);
+    }
+
+
+    [TestCase("[[[[[9,8],1],2],3],4]", null)]
+    [TestCase("[7,[6,[5,[4,[3,2]]]]]", "4")]
+    [TestCase("[[6,[5,[4,[3,2]]]],1]", "4")]
+    [TestCase("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "1")]
+    [TestCase("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "4")]
+    public void finds_left_value(string input, string expected)
+    {
+        var solver = Solver();
+        var main = solver.Parse(input);
+        var pairToExplode = main.SearchExplode();
+        var left = pairToExplode.SearchLeftValue(pairToExplode, new());
+        if (expected is null)
+            left.Should().BeNull();
+        else
+            left.ToString().Should().Be(expected);
+    }
+
+    [TestCase("[[[[[9,8],1],2],3],4]", "1")]
+    [TestCase("[7,[6,[5,[4,[3,2]]]]]", null)]
+    [TestCase("[[6,[5,[4,[3,2]]]],1]", "1")]
+    [TestCase("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "6")]
+    [TestCase("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", null)]
+    public void finds_right_value(string input, string expected)
+    {
+        var solver = Solver();
+        var main = solver.Parse(input);
+        var pairToExplode = main.SearchExplode();
+        var left = pairToExplode.SearchRightValue(pairToExplode, new());
+        if (expected is null)
+            left.Should().BeNull();
+        else
+            left.ToString().Should().Be(expected);
+    }
+
+
+    [TestCase("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+    [TestCase("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+    [TestCase("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+    [TestCase("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")]
+    [TestCase("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+    public void explodes(string input, string expected)
+    {
+        var solver = Solver();
+        var main = solver.Parse(input);
+        var exploded = main.Explode();
+        exploded.Should().BeTrue();
+        main.ToString().Should().Be(expected);
+    }
+
+    [TestCase("[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")]
+    [TestCase("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")]
+    public void splits(string input, string expected)
+    {
+        var solver = Solver();
+        var main = solver.Parse(input);
+        var split = main.Split();
+        split.Should().BeTrue();
+        main.ToString().Should().Be(expected);
+    }
+
+    [TestCase(@"[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+    [TestCase(@"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]", "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")]
+    [TestCase(@"[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]", "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")]
+    [TestCase(@"[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]", "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]", "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]")]
+    [TestCase(@"[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]", "[7,[5,[[3,8],[1,4]]]]", "[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]")]
+    [TestCase(@"[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]", "[[2,[2,2]],[8,[8,1]]]", "[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]")]
+    [TestCase(@"[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]", "[2,9]", "[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]")]
+    [TestCase(@"[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]", "[1,[[[9,3],9],[[9,0],[0,7]]]]", "[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]")]
+    [TestCase(@"[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]", "[[[5,[7,4]],7],1]", "[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]")]
+    [TestCase(@"[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]", "[[[[4,2],2],6],[8,7]]", "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")]
+    public void adds_two(string one, string two, string expected)
+    {
+        var solver = Solver();
+        var left = solver.Parse(one);
+        var right = solver.Parse(two);
+
+        var result = left.SumAndReduce(right);
+        result.ToString().Should().Be(expected);
+    }
+
+
+    [TestCase(@"[1,1]
+[2,2]
+[3,3]
+[4,4]", "[[[[1,1],[2,2]],[3,3]],[4,4]]")]
+    [TestCase(@"[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]", "[[[[3,0],[5,3]],[4,4]],[5,5]]")]
+    [TestCase(@"[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]", "[[[[5,0],[7,4]],[5,5]],[6,6]]")]
+    [TestCase(@"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]", "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")]
+    public void adds_many(string lines, string expected)
+    {
+        var solver = Solver();
+        var input = lines.Split(Environment.NewLine).Select(x => solver.Parse(x));
+        var result = solver.SumsMany(input.ToArray());
+        result.ToString().Should().Be(expected);
+    }
+
+
+    [TestCase("[9,1]", 29)]
+    [TestCase("[[9,1],[1,9]]", 129)]
+    [TestCase("[[1,2],[[3,4],5]]", 143)]
+    [TestCase("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384)]
+    [TestCase("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445)]
+    [TestCase("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
+    [TestCase("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
+    [TestCase("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
+    [TestCase("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", 4140)]
+    public void counts_magnitude(string input, long expected)
+    {
+        var solver = Solver();
+        var root = solver.Parse(input);
+        root.Magnitude().Should().Be(expected);
+    }
+
+    [Test]
     public void solves_example()
     {
         var result = Solver().Solve(Example);
-        result.Should().Be(0);
+        result.Should().Be(4140);
     }
 
-    //[Test]
+    [Test]
     public void solves_task()
     {
         var result = Solver().Solve(Helper.ReadFile(Day));
-        result.Should().Be(0);
+        result.Should().Be(4173);
     }
 }
--- AdventOfCode2021/Day18Task1.cs
+++ AdventOfCode2021/Day18Task1.cs
@@ -1,8 +1,358 @@
 ﻿namespace AdventOfCode2021;
 public class Day18Task1
 {
-    public int Solve(string input)
+    public interface IElement
     {
-        return -1;
+        int Level { get; set; }
+        IElement? Left { get; set; }
+        IElement? Right { get; set; }
+        Pair? Parent { get; set; }
+        IElement Add(IElement element);
+        (int count, int maxLevel) Count();
+
+        Pair Plus(IElement added);
+        void SetLevel(int level);
+        long Magnitude();
+
+        Pair? SearchExplode();
+        Value? SearchLeftValue(IElement explodePair, HashSet<IElement> visited);
+        Value? SearchRightValue(IElement explodePair, HashSet<IElement> visited);
+    }
+
+    public class Pair : IElement
+    {
+        public IElement? Left { get; set; }
+        public IElement? Right { get; set; }
+        public Pair? Parent { get; set; }
+
+        public int Level { get; set; }
+
+        public IElement Add(IElement element)
+        {
+            if (Left is null)
+                Left = element;
+            else if (Right is null)
+                Right = element;
+            else throw new InvalidOperationException();
+
+            element.Parent = this;
+            element.Level = Level + 1;
+
+            return element;
+        }
+
+        public (int count, int maxLevel) Count()
+        {
+            if (Left is null && Right is null)
+                return (1, Level);
+
+            (int count, int maxLevel) left = default;
+            (int count, int maxLevel) right = default;
+
+            if (Left is not null)
+                left = Left.Count();
+            if (Right is not null)
+                right = Right.Count();
+
+            return (left.count + right.count + 1, left.maxLevel > right.maxLevel ? left.maxLevel : right.maxLevel) ;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Left},{Right}]";
+        }
+
+        public Pair Plus(IElement added)
+        {
+            var newPair = new Pair();
+            newPair.Add(this);
+            newPair.Add(added);
+            this.SetLevel(newPair.Level + 1);
+            added.SetLevel(newPair.Level + 1);
+            return newPair;
+        }
+
+        public void SetLevel(int level)
+        {
+            Level = level;
+            Left?.SetLevel(level + 1);
+            Right?.SetLevel(level + 1);
+        }
+
+        public Pair? SearchExplode()
+        {
+            if (Level == 4)
+                return this;
+
+            var explode = Left?.SearchExplode();
+            if (explode is not null)
+                return explode;
+            explode = Right?.SearchExplode();
+            if (explode is not null)
+                return explode;
+
+            return null;
+        }
+
+        public bool Explode()
+        {
+            var toExplode = SearchExplode();
+            if (toExplode is null)
+                return false;
+
+            var leftValue = toExplode.SearchLeftValue(toExplode, new());
+            var rightValue = toExplode.SearchRightValue(toExplode, new());
+
+            if (leftValue is not null)
+                leftValue.Number += (toExplode.Left as Value)?.Number ?? throw new InvalidOperationException("Explode - left value");
+
+            if (rightValue is not null)
+                rightValue.Number += (toExplode.Right as Value)?.Number ?? throw new InvalidOperationException("Explode - right value");
+
+            var zero = new Value() { Level = toExplode.Level, Parent = toExplode.Parent };
+            if (toExplode.Parent.Left == toExplode)
+                toExplode.Parent.Left = zero;
+            else if (toExplode.Parent.Right == toExplode)
+                toExplode.Parent.Right = zero;
+
+            return true;
+        }
+
+        public Value? SearchLeftValue(IElement explodePair, HashSet<IElement> visited)
+        {
+            visited.Add(this);
+
+            if (this == explodePair)
+            {
+                return (Parent as Pair)?.SearchLeftValue(explodePair, visited);
+            } 
+
+            if (visited.Contains(Left))
+            {
+                return (Parent as Pair)?.SearchLeftValue(explodePair, visited);
+            }
+            else if (visited.Contains(Right))
+            {
+                var v = Left.SearchLeftValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                return (Parent as Pair)?.SearchLeftValue(explodePair, visited);
+            }
+            else
+            {
+                var v = Right.SearchLeftValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                v = Left.SearchLeftValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                return (Parent as Pair)?.SearchLeftValue(explodePair, visited);
+            }
+        }
+
+        public Value? SearchRightValue(IElement explodePair, HashSet<IElement> visited)
+        {
+            visited.Add(this);
+
+            if (this == explodePair)
+            {
+                return (Parent as Pair)?.SearchRightValue(explodePair, visited);
+            }
+
+            if (visited.Contains(Right))
+            {
+                return (Parent as Pair)?.SearchRightValue(explodePair, visited);
+            }
+            else if (visited.Contains(Left))
+            {
+                var v = Right.SearchRightValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                return (Parent as Pair)?.SearchRightValue(explodePair, visited);
+            }
+            else
+            {
+                var v = Left.SearchRightValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                v = Right.SearchRightValue(explodePair, visited);
+                if (v is not null)
+                    return v;
+                return (Parent as Pair)?.SearchRightValue(explodePair, visited);
+            }
+        }
+
+        public bool Split()
+        {
+            Value? toSplit = SearchSplit();
+            if (toSplit is null)
+                return false;
+
+            var left = toSplit.Number / 2;
+            var right = left + (toSplit.Number % 2);
+
+            var pair = new Pair() { Level = toSplit.Level, Parent = toSplit.Parent };
+            pair.Add(new Value { Number = left });
+            pair.Add(new Value { Number = right });
+
+            if (toSplit.Parent?.Left == toSplit)
+                toSplit.Parent.Left = pair;
+            else if (toSplit.Parent?.Right == toSplit)
+                toSplit.Parent.Right = pair;
+            else throw new InvalidOperationException();
+
+            return true;
+        }
+
+        public Value? SearchSplit()
+        {
+            Value? toSplit = null;
+            if (Left is Value val && (val.Number > 9))
+                return val;
+            else if (Left is Pair p)
+                toSplit = p.SearchSplit();
+            
+            if (toSplit is not null)
+                return toSplit;
+
+            if (Right is Value val2 && (val2.Number > 9))
+                return val2;
+            else if (Right is Pair p)
+                toSplit = p.SearchSplit();
+
+            return toSplit; // Value or null
+        }
+
+        public Pair SumAndReduce(Pair right)
+        {
+            var sum = Plus(right);
+            sum.Reduce();
+            return sum;
+        }
+
+        public void Reduce()
+        {
+            while (true)
+            {
+                if (this.Explode())
+                    continue;
+                if (this.Split())
+                    continue;
+                break;
+            }
+        }
+
+        public long Magnitude()
+        {
+            return 3 * Left.Magnitude() + 2 * Right.Magnitude();
+        }
+    }
+
+    public class Value : IElement
+    {
+        public long Number;
+
+        public int Level { get; set; }
+        public Pair? Parent { get; set; }
+        public IElement? Left { get; set; }
+        public IElement? Right { get; set; }
+
+        public IElement Add(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public (int count, int maxLevel) Count()
+        {
+            return (1, Level);
+        }
+
+        public long Magnitude()
+        {
+            return Number;
+        }
+
+        public Pair Plus(IElement added)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pair? SearchExplode()
+        {
+            return null;
+        }
+
+        public Value? SearchLeftValue(IElement explodePair, HashSet<IElement> visited)
+        {
+            return this;
+        }
+
+        public Value? SearchRightValue(IElement explodePair, HashSet<IElement> visited)
+        {
+            return this;
+        }
+
+        public void SetLevel(int level)
+        {
+            Level = level;
+        }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+
+    public Pair Parse(ReadOnlySpan<char> input)
+    {
+        var stack = new Stack<IElement>();
+        IElement current = new Pair();
+        var level = 0;
+        for(var index = 1; index < input.Length - 1; index++)
+        {
+            var c = input[index];
+            if (c == '[')
+            {
+                level++;
+                stack.Push(current);
+                current = current.Add(new Pair());
+            }
+            else if (c == ']')
+            {
+                level--;
+                current = stack.Pop();
+            }
+            else if (c == ',') continue;
+            else // read numver while ',' or ']'
+            {
+                var sb = new StringBuilder();
+                while (input[index] != ',' && input[index] != ']')
+                {
+                    sb.Append(input[index++]);
+                }
+                if (input[index] == ']') index--;
+                current.Add(new Value() { Number = long.Parse(sb.ToString()) });
+            }
+        }
+
+        return current as Pair;
+    }
+
+    public Pair SumsMany(params Pair[] elements)
+    {
+        if (elements.Length < 2)
+            throw new ArgumentException("too few elements in parameter", nameof(elements));
+        var result = elements[0];
+        for(var i = 1; i < elements.Length; i++)
+            result = result.SumAndReduce(elements[i]);
+
+        return result;
+    }
+
+    public long Solve(string input)
+    {
+        var pairs = input.Split(Environment.NewLine).Select(x => Parse(x)).ToArray();
+        var result = SumsMany(pairs);
+        return result.Magnitude();
     }
 }
