93bd9074330675460604ca3b97261397ef94a8ee
Mateusz <mateuszmaj@o2.pl>
2021-12-24 13:37:18 +0100

    Day 21 Task 1

--- AdventOfCode2021.Tests/Day21Task1Tests.cs
+++ AdventOfCode2021.Tests/Day21Task1Tests.cs
@@ -1,18 +1,19 @@
 ﻿namespace AdventOfCode2021.Tests;
 internal class Day21Task1Tests
 {
-    public string Example = @"";
+    public string Example = @"Player 1 starting position: 4
+Player 2 starting position: 8";
     Day21Task1 Solver() => new();
     int Day = 21;
 
-    //[Test]
+    [Test]
     public void solves_example()
     {
         var result = Solver().Solve(Example);
-        result.Should().Be(0);
+        result.Should().Be(739785);
     }
 
-    //[Test]
+    [Test]
     public void solves_task()
     {
         var result = Solver().Solve(Helper.ReadFile(Day));
--- AdventOfCode2021/Day21Task1.cs
+++ AdventOfCode2021/Day21Task1.cs
@@ -1,8 +1,47 @@
-﻿namespace AdventOfCode2021;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021;
 public class Day21Task1
 {
-    public int Solve(string input)
+    public long Solve(string input)
     {
-        return -1;
+        var inputLines = input.Split(Environment.NewLine);
+        var pos1 = int.Parse(Regex.Match(inputLines[0], @"^Player 1 starting position: (\d+)$").Groups[1].Value);
+        var pos2 = int.Parse(Regex.Match(inputLines[1], @"^Player 2 starting position: (\d+)$").Groups[1].Value);
+        var dice = InitDice().ToArray();
+        var (score1, score2) = (0, 0);
+        var isPlayerOneTurn = true;
+        var diceRoll = 0;
+        while(score1 < 1000 && score2 < 1000)
+        {
+            var rollValue = dice[diceRoll % dice.Length];
+
+            if (isPlayerOneTurn)
+            {
+                pos1 = (pos1 - 1 + rollValue) % 10 + 1;
+                (score1, score2) = (score1 + pos1, score2);
+            }
+            else
+            {
+                pos2 = (pos2 - 1 + rollValue) % 10 + 1;
+                (score1, score2) = (score1, score2 + pos2);
+            }
+            diceRoll++;
+            isPlayerOneTurn = !isPlayerOneTurn;
+        }
+
+        return (score1 < score2 ? score1 : score2) * diceRoll * 3;
+    }
+
+    public IEnumerable<int> InitDice()
+    {
+        var i = 0;
+        do
+        {
+            var (a, b, c) = ((i % 100) + 1, ((i + 1) % 100) + 1, ((i + 2) % 100 + 1));
+            yield return a + b + c;
+            i = (i + 3) % 100;
+        }
+        while (i != 0);
     }
 }
